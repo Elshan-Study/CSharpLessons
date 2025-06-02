b7384c822ede8372ccfa1d0cc5fe81f129ba8e4d
Elshan <elshanisayev378@gmail.com>
2025-06-02 11:39:39 +0400

    lesson 8-9 added

--- CSharpLesson/Employee.cs
+++ CSharpLesson/Employee.cs
@@ -1,21 +1,21 @@
 namespace CSharpLesson;
 
-public class Employee
-{
-    public string Name { get; set; }
-    public float Salary { get; set; }
-    
-    public List<TaxDelegate>? Taxes { get; set; }
-
-    // public float CalculateAllTaxes()
-    // {
-    //    return Taxes?.Sum(tax => tax(Salary)) ?? 0f;
-    // }
-    
-    public Employee(string name, float salary, List<TaxDelegate>? taxes=null)
-    {
-        Name = name;
-        Salary = salary;
-        Taxes = taxes;
-    }
-}
+// public class Employee
+// {
+//     public string Name { get; set; }
+//     public float Salary { get; set; }
+//     
+//     public List<TaxDelegate>? Taxes { get; set; }
+//
+//     // public float CalculateAllTaxes()
+//     // {
+//     //    return Taxes?.Sum(tax => tax(Salary)) ?? 0f;
+//     // }
+//     
+//     public Employee(string name, float salary, List<TaxDelegate>? taxes=null)
+//     {
+//         Name = name;
+//         Salary = salary;
+//         Taxes = taxes;
+//     }
+// }
---
+++ CSharpLesson/Lesson 8.cs
@@ -0,0 +1,9 @@
+namespace CSharpLesson;
+
+// MyClass<int> myClass = new();
+// MyClass<string> myClass2 = new();
+//
+// class MyClass<T>
+// {
+//     public static T a;
+// }
